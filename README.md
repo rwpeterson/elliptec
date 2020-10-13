19563d84e2b922c58f833e022543febff5d02387
Bob Peterson <bob@rwp.is>
2020-10-13 21:46:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ # ThorlabsELLx
+
+ Python interface to Thorlabs Elliptec motorized mounts
+
+ ## Initial work
+
+ Initial work is only on supporting the ELL14 rotation mount
---
+++ ThorlabsELLx.py
@@ -0,0 +1,56 @@
+import io
+import serial
+
+def elliptec_open(dev):
+    ser = serial.Serial(dev,
+                        9600,
+                        bytesize=serial.EIGHTBITS,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        timeout=1)
+    return ser
+
+def elliptec_buffer(ser):
+    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser),
+                           newline='\r\n')
+    return sio
+
+def bufmsg(sio, msg):
+    sio.write(msg)
+    sio.flush()
+    return sio.readline()
+
+def emsg(sio, addr, msg):
+    addr = str(int(addr, 16))
+    return bufmsg(sio, addr + msg)
+
+def eident(sio, addr):
+    return emsg(sio, addr,'in')
+
+def emotorinfo(sio, addr):
+    i1 = emsg(sio, addr, 'i1')
+    i2 = emsg(sio, addr, 'i2')
+    return i1 + i2
+
+def ehomeoff(sio, addr):
+    return emsg(sio, addr, 'go')
+
+def ejogstep(sio, addr):
+    return emsg(sio, addr, 'gj')
+
+def epos(sio, addr):
+    return emsg(sio, addr, 'gp')
+
+def ehome(sio, addr):
+    return emsg(sio, addr, 'ho1')
+
+def deg2estep(deg):
+    return int(deg * 262144/360)
+
+def estep2ehex(step):
+    return hex(step)[2:].zfill(8)
+
+def eabsm(sio, addr, deg):
+    step = deg2estep(deg)
+    hstep = estep2ehex(step)
+    return emsg(sio, addr, 'ma' + hstep)
