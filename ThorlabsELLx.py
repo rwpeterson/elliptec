f7256a0d005fd51ebb0ee61dafcb87705d667c9c
Bob Peterson <bob@rwp.is>
2020-10-16 12:53:14 +0200

    Added docstrings

--- ThorlabsELLx.py
+++ ThorlabsELLx.py
@@ -3,6 +3,7 @@ import serial
 
 
 class ThorlabsELLx:
+    '''Thorlabs ELLx controller, can address up to 16 motors'''
     def __init__(self, dev, addrs):
         self.ser = self.openserial(dev)
         self.sio = self.openbuffer(self.ser)
@@ -15,6 +16,7 @@ class ThorlabsELLx:
                 self.scaling[addr] = int(info.strip()[-8:],16)
 
     def close(self):
+        '''Shut down the buffer and serial connection cleanly.'''
         # Still not sure how to sequentially close these without errors
         self.sio.close()
         self.ser.close()
@@ -34,15 +36,19 @@ class ThorlabsELLx:
         return sio
 
     def bufmsg(self, msg):
+        '''Primitive to send message to controller and return response.'''
         self.sio.write(msg)
         self.sio.flush()
         return self.sio.readline()
 
     def msg(self, addr, msg):
+        '''Convenience function for commands which begin with address.'''
         addr = str(int(addr, 16))
         return self.bufmsg(self.sio, addr + msg)
 
     def ident(self, addr):
+        '''Get basic identification from motor.
+        Can be used to check if motor is connected.'''
         return self.msg(addr, 'in')
 
     def motorinfo(self, addr):
@@ -51,24 +57,37 @@ class ThorlabsELLx:
         return i1 + i2
 
     def homeoff(self, addr):
+        '''Requests the motor's home position, relative to the absolute
+        limit of travel.
+        Note: Thorlabs warns NOT to set the home position to a custom
+              value, since it is factory-set to ensure consistent
+              homing.'''
         return self.msg(addr, 'go')
 
     def jogstep(self, addr):
+        '''Requests jog length.'''
         return self.msg(addr, 'gj')
 
     def pos(self, addr):
+        '''Requests current motor position.'''
         return self.msg(addr, 'gp')
 
     def home(self, addr):
+        '''Move motor to home position. For rotary stages, byte 3 sets
+        direction: 0 CW and 1 CCW.'''
         return self.msg(addr, 'ho1')
 
     def deg2step(self, addr, deg):
+        '''Use scaling factor queried from motor during init.'''
         return int(deg * self.scaling[addr]/360)
 
     def step2hex(self, step):
+        '''Controller accepts hex postions, note that [a-f] are NOT
+        accepted as valed hex values.'''
         return hex(step)[2:].zfill(8).upper()
 
     def absm(self, addr, deg):
+        '''Move motor to specified absolute position.'''
         step = self.deg2step(addr, deg)
         hstep = self.step2hex(step)
         return self.msg(addr, 'ma' + hstep)
