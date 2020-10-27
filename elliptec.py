2d8849801da19e135c3bde730b86114dc81e8afe
Bob Peterson <bob@rwp.is>
2020-10-27 12:04:37 +0100

    Lint elliptec.py - using flake8 and numpy docstring convention

--- elliptec.py
+++ elliptec.py
@@ -4,44 +4,53 @@ import io
 import serial
 
 # Error codes for controller
-OK               = 00
-COMM_TIMEOUT     = 01
-MECH_TIMEOUT     = 02
-COMMAND_ERR      = 03
-VAL_OUT_OF_RANGE = 04
-MOD_ISOLATED     = 05
-MOD_OUT_OF_ISOL  = 06
-INIT_ERROR       = 07
-THERMAL_ERROR    = 08
-BUSY             = 09
-SENSOR_ERROR     = 10
-MOTOR_ERROR      = 11
-OUT_OF_RANGE     = 12
-OVER_CURRENT     = 13
-GENERAL_ERROR    = 14
+OK = 0
+COMM_TIMEOUT = 1
+MECH_TIMEOUT = 2
+COMMAND_ERR = 3
+VAL_OUT_OF_RANGE = 4
+MOD_ISOLATED = 5
+MOD_OUT_OF_ISOL = 6
+INIT_ERROR = 7
+THERMAL_ERROR = 8
+BUSY = 9
+SENSOR_ERROR = 10
+MOTOR_ERROR = 11
+OUT_OF_RANGE = 12
+OVER_CURRENT = 13
+GENERAL_ERROR = 14
 
 # Error handling modes
 PASS = 0
 ERROR = 1
 LAZY = 2
 
+
 class Error(Exception):
-    """Base class"""
+    """Base class."""
+
     pass
+
 
 class ElliptecError(Error):
-    """Reply to command indicates an error has occured"""
+    """Reply to command indicates an error has occured."""
+
     pass
 
+
 class Elliptec:
-    """Initialize serial communication with the elliptec controller, and
-    probe the modules at the supplied addresses to gather information and
-    perform an intial homing."""
+    """Create class to manage Elliptec controller and connected modules.
+
+    Class initializes serial communication with the elliptec controller, and
+    probes the modules at the supplied addresses to gather information and
+    perform an intial homing.
+    """
+
     def __init__(self, dev, addrs, hmode=PASS):
         self.openserial(dev)
         self.openbuffer()
-        self.info = {}
-        self.zero = {}
+        self.info = dict()
+        self.zero = dict()
         self.hmode = hmode
         self.hct = 0
         self.sct = 0
@@ -59,40 +68,31 @@ class Elliptec:
                 # datum for subsequent moving
                 self.home(addr)
 
-    def handler(self, retval, statusbusy, statusfinal):
-        """Processes replies from modules, optionally handling errors
-        and deferring resolution of busy modules until later.
-        
-        ## Modes
+    def handler(self, retval):
+        """Process replies from modules according to error mode.
 
-        PASS: After every command, wait on readline() to capture one
-        line of reply. Then pass it without checking for an error. This
-        is a crude solution for commands which always result in one
-        reply, blocking until that reply is received. It will cause
-        problems if multiple lines are received per command. Examples
-        include errors encountered and reported autonomously by the
-        modules, and the result of commands like groupaddress that cause
-        multiple modules to obey (and respond) to a single command.
+        By setting hmode in the Elliptec() class, handle errors returned
+        vis the modules accoring to the following modes.
 
-        ERROR: Same as PASS, but raise an error if the reply is
-        not as expected.
-
-        LAZY: Send commands, keep track of the expected number of
-        replies but wait until a user-defined time to process them
-        (and possibly check for additional errors)
-
-        
-        
+        Modes
+        -----
+        PASS
+            After every command, wait on readline() to capture one
+            line of reply. Then pass it without checking for an error. This
+            is a crude solution for commands which always result in one
+            reply, blocking until that reply is received. It will cause
+            problems if multiple lines are received per command. Examples
+            include errors encountered and reported autonomously by the
+            modules, and the result of commands like groupaddress that cause
+            multiple modules to obey (and respond) to a single command.
+        LAZY
+            Send commands, keep track of the expected number of
+            replies but wait until a user-defined time to process them
+            (and possibly check for additional errors)
         """
-        if hmode == PASS:
+        if self.hmode == PASS:
             return retval
-        elif hmode == ERROR:
-            status = self.parsestatus(retval)
-            if status != statusbusy or status != statusfinal:
-                raise ElliptecError
-            else
-                return retval
-        elif hmode == LAZY:
+        elif self.hmode == LAZY:
             return
 
     def readmsgs(self):
@@ -102,7 +102,6 @@ class Elliptec:
             msgs.append(self.sio.readline())
         self.sct = 0
         return msgs
-            
 
     def openserial(self, dev):
         """Open serial connection."""
@@ -135,21 +134,21 @@ class Elliptec:
         self.sio.write(msg)
 
     def msg(self, addr, msg):
-        """Send message to module, handling reply according to hmode"""
+        """Send message to module, handling reply according to hmode."""
         addr = str(int(addr, 16))
         if self.hmode == PASS or self.hmode == ERROR:
             return self.bufmsg(addr + msg)
         elif self.hmode == LAZY:
             self.smsg += 1
-            return self.sndmsg(addr + msg)
+            self.sndmsg(addr + msg)
 
     def getinfo(self, addr):
         """Get information about module.
-        
+
         Reply format:
         | 0 | 1-2  | 3-4 | 5-12 | 13-16 | 17-18 | 19-20 | 21-24  | 25-32  |
         | A | "IN" | ELL | SN   | YEAR  | FWREL | HWREL | TRAVEL | PULSES |
-        
+
         Definitions:
             A      - address
             "IN"   - command name
@@ -174,28 +173,21 @@ class Elliptec:
         self.info[addr]["fwrel"] = info.strip()[17:19]
         self.info[addr]["hwrel"] = info.strip()[19:21]
         self.info[addr]["travel"] = info.strip()[21:25]
-        self.info[addr]["pulses"] = int(info.strip()[25:33]
-        return
-
-    def motorinfo(self, addr):
-        """more detailed motor information."""
-        i = self.msg(addr, 'i1')
-        j = self.msg(addr, 'i2')
-        return self.handler(i + j)
+        self.info[addr]["pulses"] = int(info.strip()[25:33])
 
     def getmotorinfo1(self, addr):
         """Get motor 1 parameters from module.
 
         Reply format:
-        | 0 | 1-2  | 3    | 4     | 5-8     | 9-12   | 13-16  | 17-20  | 21-24  |
-        | A | "I1" | LOOP | MOTOR | CURRENT | RAMPUP | RAMPDN | FWDPER | BAKPER |
+        | 0 | 1-2 | 3    | 4     | 5-8   | 9-12   | 13-16  | 17-20  | 21-24  |
+        | A | I1  | LOOP | MOTOR | CRRNT | RAMPUP | RAMPDN | FWDPER | BAKPER |
 
         Definitions:
         A - address
         "I1" - command name
         LOOP - state of loop (1 = ON)
         MOTOR - state of motor (1 = ON)
-        CURRENT - 1866 points is 1 amp
+        CRRNT - 1866 points is 1 amp
         RAMPUP - PWM increase every ms (0xFFFF undef.)
         RAMPDN - PWN decrease every ms
         FWDPER - forward period value
@@ -205,61 +197,77 @@ class Elliptec:
         return self.handler(self.msg(addr, 'i1'))
 
     def getmotorinfo2(self, addr):
-        """Get motor 2 parameters from module, for devices which have two motors."""
+        """Get motor 2 parameters from module.
+
+        Only applies for devices which have two motors.
+        """
         return self.handler(self.msg(addr, 'i2'))
 
-    def storemotorinfo(self, num, m):
-        """Parses and stores motor info for motor <num>."""
-        self.info[addr][num] = {}
-        self.info[addr][num]["loop"]           = int(m.strip()[3:4])
-        self.info[addr][num]["motor"]          = int(m.strip()[4:5])
-        self.info[addr][num]["current"]        = int(m.strip()[5:9], 16) / 1866
-        self.info[addr][num]["rampup"]         = int(m.strip()[9:13], 16)
-        self.info[addr][num]["rampdown"]       = int(m.strip()[13:17], 16)
-        self.info[addr][num]["forwardperiod"]  = 14740000 / int(m.strip()[17:21], 16)
-        self.info[addr][num]["backwardperiod"] = 14740000 / int(m.strip()[21:25], 16)
-    
+    def storemotorinfo(self, addr, num, m):
+        """Parse and store motor info for motor `num`."""
+        self.info[addr][num] = dict()
+        self.info[addr][num]["loop"] = int(m.strip()[3:4])
+        self.info[addr][num]["motor"] = int(m.strip()[4:5])
+        self.info[addr][num]["current"] = int(m.strip()[5:9], 16) / 1866
+        self.info[addr][num]["rampup"] = int(m.strip()[9:13], 16)
+        self.info[addr][num]["rampdown"] = int(m.strip()[13:17], 16)
+        self.info[addr][num]["forwardperiod"] = \
+            14740000 / int(m.strip()[17:21], 16)
+        self.info[addr][num]["backwardperiod"] = \
+            14740000 / int(m.strip()[21:25], 16)
+
     def changeaddress(self, addr, naddr):
-        """Change address of module at addr to naddr. Expected reply: GS00 from new address."""
-        return self.handler(self.msg(addr, 'ca' + naddr), )
+        """Change address of module at addr to naddr.
+
+        Expected reply: GS00 from new address.
+        """
+        return self.handler(self.msg(addr, 'ca' + naddr))
 
     def groupaddress(self, addr, gaddr):
-        """instruct module at addr to respond to the next command sent
-        to gaddr, in order to operate simultaneously. it will switch
-        back to responding to its own address after one operation.
-        Responses from multiple devices are ordered by their address,
-        with 0x0 having priority."""
+        """Instruct module at `addr` to respond to `gaddr`.
+
+        The next command sent to `gaddr` will be obeyed by the module at
+        `addr`, in order to operate simultaneously. It will switch back
+        to responding to its own address after one operation. Responses
+        from multiple devices are ordered by their address, with 0x0
+        having priority.
+        """
         return self.handler(self.msg(addr, 'ga' + gaddr))
 
     def cleanmechanics(self, addr):
-        """Perform cleaning cycle on module. Note: takes several
-        minutes and will block other commands, replying with busy.
-        Other modules on the same bus may have performance affected
-        during this time."""
+        """Perform cleaning cycle on module.
+
+        Note: takes several minutes and will block other commands,
+              replying with busy. Other modules on the same bus may have
+              performance affected during this time.
+        """
         return self.handler(self.msg(addr, 'cm'))
 
     def homeoff(self, addr):
-        """Requests the motor's home position, relative to the absolute
-        limit of travel.
+        """Request the motor's home position.
+
         Note: Thorlabs warns NOT to set the home position to a custom
               value, since it is factory-set to ensure consistent
               homing. For the ELL14 rotation mount, the position can
               only be changed up to 90 deg. See the setcal/cal* methods
               for a convenient way to set an arbitrary offset for
-              successive movements."""
+              successive movements.
+        """
         return self.handler(self.msg(addr, 'go'))
 
     def jogstep(self, addr):
-        """Requests jog length."""
+        """Request jog length."""
         return self.handler(self.msg(addr, 'gj'))
 
     def pos(self, addr):
-        """Requests current motor position."""
+        """Request current motor position."""
         return self.handler(self.msg(addr, 'gp'))
 
     def home(self, addr):
-        """Move motor to home position. For rotary stages, byte 3 sets
-        direction: 0 CW and 1 CCW."""
+        """Move motor to home position.
+
+        For rotary stages, byte 3 sets direction: 0 CW and 1 CCW.
+        """
         return self.handler(self.msg(addr, 'ho1'))
 
     def deg2step(self, addr, deg):
@@ -268,8 +276,10 @@ class Elliptec:
 
     @staticmethod
     def step2hex(step):
-        """Controller accepts hex postions, note that [a-f] are NOT
-        accepted as valed hex values."""
+        """Convert int to hex-encoded string understood by controller.
+
+        Note that [a-f] are NOT accepted as valed hex values.
+        """
         return hex(step)[2:].zfill(8).upper()
 
     def absm(self, addr, deg):
@@ -279,10 +289,12 @@ class Elliptec:
         return self.handler(self.msg(addr, 'ma' + hstep))
 
     def setcal(self, addrs, angles):
-        """Set a calibration offset for motor at addr, so that the cal-
-        series of commands will move relative to this offset from the
+        """Set a calibration offset for motor at `addr`.
+
+        The cal- series of commands will move relative to this offset from the
         home position. Accepts an individual address and angle, or lists
-        of both."""
+        of both.
+        """
         try:
             for addr, angle in zip(addrs, angles):
                 self.zero[addr] = angle
@@ -291,7 +303,9 @@ class Elliptec:
 
     def calmove(self, addrs, angles):
         """Move motor at addr to angle relative to calibration offset.
-        Accepts an individual address and angle, or lists of both."""
+
+        Accepts an individual address and angle, or lists of both.
+        """
         try:
             for addr, angle in zip(addrs, angles):
                 self.absm(addr, self.zero[addr] + angle)
